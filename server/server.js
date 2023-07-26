54826c19fdb7aad69155c1c35e29c80c444906a0
Joi Misenti <joipolloi1@gmail.com>
2023-07-26 17:57:02 -0400

    starting deployment lab

---
+++ server/server.js
@@ -0,0 +1,11 @@
+// first run npm init -y which will make a package.json file
+// then run npm i express to get the express package installed
+// then set up a git to track changes by sharing local code with remote server
+
+const express = require('express')
+const app = express()
+
+app.use(express.static(`${__dirname}/public`))
+
+
+app.listen(4000, () => console.log(`server running on 4000`))
